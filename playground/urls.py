cd9ff30600ce6e00dac859f73b3388d2ec0512f5
AndyDickie <89486332+AndyDickie@users.noreply.github.com>
2022-04-01 14:29:51 +0200

    Ny start

--- playground/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('hello/', views.say_hello, name='hello'),
-    path('login/', views.sut, name='login'),
-    path('home/', views.home_screen, name='home'),
-    path('', views.redirect_to_home)
-]
