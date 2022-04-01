f89dfc1fe0b01229e792728f23536ee030ee399c
AndyDickie <89486332+AndyDickie@users.noreply.github.com>
2022-04-01 15:43:55 +0200

    1/2 reset password

--- Wordle/Wordle/settings.py
+++ Wordle/Wordle/settings.py
@@ -125,3 +125,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/" 
+
+#Til at reset kode
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
+
