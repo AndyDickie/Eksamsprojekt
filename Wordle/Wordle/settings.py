ee611c22eeaa7a8c6adfe2c14a24c674d1b0abc3
AndyDickie <89486332+AndyDickie@users.noreply.github.com>
2022-04-05 10:06:03 +0200

    Forhåbentlig fix

--- Wordle/Wordle/settings.py
+++ Wordle/Wordle/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'Wordle.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/Users/andydickie/Documents/GitHub/Eksamsprojekt/Untitled/Wordle/db.sqlite3',
+        'NAME': '/Users/andydickie/Documents/GitHub/Eksamsprojekt/Wordle/db.sqlite3',
     }
 }
 
