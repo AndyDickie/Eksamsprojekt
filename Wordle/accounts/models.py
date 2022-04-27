d3873ce3d1415aef1aa152dd9e3724715e25770d
AndyDickie <89486332+AndyDickie@users.noreply.github.com>
2022-04-27 14:03:40 +0200

    updates to gui

--- Wordle/accounts/models.py
+++ Wordle/accounts/models.py
@@ -10,19 +10,28 @@ class FriendList(models.Model):
     #Vennelisten er many to many, da det indeholder flere users (som venner)
     friends = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='friends')
 
+    #Listen over blokerede brugere er et many to many forhold
+    blocked_users = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='blocked_users')
+
    
     def add_friend(self, account):
-
         if not account in self.friends.all():
             self.friends.add(account)
+            if account in self.blocked_users.all():
+                self.blocked_users.remove(account)
             self.save()
-    
 
     def remove_friend(self, account):
-
         if account in self.friends.all():
             self.friends.remove(account)
             self.save()
+    
+    def block_user(self, account):
+        self.blocked_users.add(account)
+        if account in self.friends.all():
+            self.friends.remove(account)
+            self.save()
+        pass
 
     def __str__(self):
         return(self.user.username)
