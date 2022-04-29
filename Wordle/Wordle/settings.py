a4035829c8cac8758c7fe65391a269e6193f37d4
joha7809 <joha7809@gmail.com>
2022-04-29 12:41:26 +0200

    Database tilgængelig for alle github repos

--- Wordle/Wordle/settings.py
+++ Wordle/Wordle/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'Wordle.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/Users/andydickie/Documents/GitHub/Eksamsprojekt/Wordle/Wordle/db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
